e900cc324e62e07cf27e563a244a46593278efff
PietraMassarotti <pietramassarotti@gmail.com>
2025-08-06 11:57:01 -0300

    Update index.php

---
+++ public/Login/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="" media="screen" />
+    <title>Login-MeuEstoqueVirtual</title>
+</head>
+<body>
+    <h1>Meu Estoque Virtual</h1>
+    <h2>Login</h2>
+    <form action="" method="POST">
+        <label>E-mail:</label>
+        <input type="email" name="email" required>
+        <label>Senha:</label>
+        <input type="password" name="senha" required>
+    </form>
+    <form action="" method="GET">
+        <button>Entrar</button>
+    </form>
+</body>
+</html>
